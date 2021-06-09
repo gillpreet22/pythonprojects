d4c5c352e04b8752de38a0f1a81c044fc165c00b
Gurpreet Gill <gurprgil@cisco.com>
2021-06-09 09:44:10 -0700

    adding DNAC file

---
+++ DNAtoken.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+base_url = "https://dcloud-dna-center-inst-rtp.cisco.com/dna/"
+auth_endpoint = "system/api/v1/auth/token"
+
+user = 'devnetuser'
+password = 'Cisco123!'
+
+auth_response = requests.post(url="https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token", auth=(user, password)).json()
+
+token = auth_response['Token']
+
+headers = {
+    "x-auth-token": token,
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+
