f945711d3ce9837aae1520471433355acd4be168
Gurpreet Gill <gurprgil@cisco.com>
2021-06-09 12:54:14 -0700

    modified file

--- DNAtoken.py
+++ DNAtoken.py
@@ -5,7 +5,7 @@ base_url = "https://dcloud-dna-center-inst-rtp.cisco.com/dna/"
 auth_endpoint = "system/api/v1/auth/token"
 
 user = 'devnetuser'
-password = '@@@@'
+password = '~@@@@'
 
 auth_response = requests.post(url="https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token", auth=(user, password)).json()
 
